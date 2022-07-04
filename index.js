9f6a3a5e91ef974e7a211bb6ea9e7fd5bcda56d4
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-04 19:43:09 -0400

    all tests pass

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+let multiply = num1 * num2;
+let mod = num3 % num4
+let random = Math.floor(Math.random() * 1000 + 1);
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
